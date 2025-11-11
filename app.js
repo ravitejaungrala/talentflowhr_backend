cda5bcf220b7ab8250c8453934c349aa75fa3c0d
UNGARALA NAGA VENKATA RAVITEJA <127975029+ravitejaungrala@users.noreply.github.com>
2025-11-12 00:03:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.use(cors({
   origin: [
     'http://localhost:3001',
     'http://localhost:5173',
-    'https://talentflowhr-frontend.netlify.app',
-    'https://talentflow-hr-frontend.netlify.app'
+    'https://talenthr-front.onrender.com/',
+    'https://talenthr-front.onrender.com'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
